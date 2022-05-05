b0c4e1e6e54c6aff2054ea33ecf003b68dfd4198
Timothy Wiemken <tim.wiemken@gmail.com>
2022-05-05 09:29:20 -0500

    updates

--- R/influenza anomaly.R
+++ R/influenza anomaly.R
@@ -95,21 +95,21 @@ df.prophet %>%
   #filter(ds < "2021-12-14") %>%
   tibble() %>%
   time_decompose(y, method = "twitter", frequency = "auto", trend = "auto") %>%
-  anomalize(remainder, method = "gesd", alpha = 0.05, max_anoms = 0.2) %>%
-  time_recompose
+  anomalize(remainder, method = "gesd", alpha = 0.05, max_anoms = 0.3) %>%
+  time_recompose() -> yo
 
-p +
-  labs(
-    x="\n Date",
-    y = "Value \n"
-  ) +
-  scale_x_date(date_breaks = "1 month", date_labels = "%b %Y", expand=c(0.01,0)) +
-  scale_y_continuous(expand=c(0.2,0.1)) +
-  theme(
-    axis.text.x = element_text(angle=90)
-  ) -> p2
+# p +
+#   labs(
+#     x="\n Date",
+#     y = "Value \n"
+#   ) +
+#   scale_x_date(date_breaks = "1 month", date_labels = "%b %Y", expand=c(0.01,0)) +
+#   scale_y_continuous(expand=c(0.2,0.1)) +
+#   theme(
+#     axis.text.x = element_text(angle=90)
+#   ) -> p2
 
-p2[[1]] -> yo
+
 
 yo$ds <- as.Date(yo$ds)
 yo$year <- lubridate::year(yo$ds)
@@ -122,23 +122,22 @@ yo %>%
     ymin = -2000,
     ymax = 0 )-> label_range
 
-
 leg <-  tibble(
-  colour = ifelse(yo[yo$key=="observed","anomaly"]=="Yes", "red", "black"),
-  size = ifelse(yo[yo$key=="observed","anomaly"]=="Yes", 1.5,1),
-  size2 = ifelse(yo[yo$key=="observed","anomaly"]=="Yes", 3,1),
-  shape = ifelse(yo[yo$key=="observed","anomaly"]=="Yes", 1,19),
-  size_disappear = ifelse(yo[yo$key=="observed","anomaly"]=="Yes", 3,1),
-  stroke = ifelse(yo[yo$key=="observed","anomaly"]=="Yes", 1.5,0.8)
+  colour = ifelse(yo[,"anomaly"]=="Yes", "red", "black"),
+  size = ifelse(yo[,"anomaly"]=="Yes", 1.5,1),
+  size2 = ifelse(yo[,"anomaly"]=="Yes", 3,1),
+  shape = ifelse(yo[,"anomaly"]=="Yes", 1,19),
+  size_disappear = ifelse(yo[,"anomaly"]=="Yes", 3,1),
+  stroke = ifelse(yo[,"anomaly"]=="Yes", 1.5,0.8)
 )
 
 # replot ----
-ggplot(data = yo[yo$key=="observed",]) + 
+ggplot(data = yo) + 
   geom_line(
-    aes(x= ds, y=value)
+    aes(x= ds, y=observed)
   ) +
   geom_point(
-    aes(x=ds, y=value, colour = leg$colour, size = leg$size_disappear, shape = leg$shape), 
+    aes(x=ds, y=observed, colour = leg$colour, size = leg$size_disappear, shape = leg$shape), 
     size = leg$size_disappear,
     shape = leg$shape,
     stroke= leg$stroke
@@ -151,10 +150,10 @@ ggplot(data = yo[yo$key=="observed",]) +
   scale_color_manual(name = "Anomaly", values = c("black" = "#272936", "red" = "#88B0AD"), labels = c("No", "Yes")) +
   geom_rect(data = label_range, fill = "#A66923", color = "#f2f2f2", alpha = 0.5,
             aes(xmin = xmin, xmax = xmax, 
-                ymin = ymin-1000, ymax = ymax-1250,
+                ymin = ymin-2000, ymax = ymax-2500,
                 group = year)) +
   geom_text(data = label_range,
-            aes(x = xmin+50, y = ymin-250,
+            aes(x = xmin+50, y = ymin-1250,
                 group = year, label = year), size=3) +
   geom_ribbon(
     aes(x= ds, ymin = recomposed_l1, ymax=recomposed_l2), color = "#272936", alpha=0.2
@@ -162,9 +161,9 @@ ggplot(data = yo[yo$key=="observed",]) +
   scale_x_date(
     date_breaks = "1 month", 
     date_labels = "%b", 
-    expand=c(0.01, 0.05)) +
+    expand=c(0.02, 0.05)) +
   scale_y_continuous(
-    limits = c(-3000, 30000), n.breaks=12
+    limits = c(-4000, 30000), n.breaks=12
   ) + 
   labs(
     colour = "Anomaly",
@@ -185,5 +184,5 @@ ggplot(data = yo[yo$key=="observed",]) +
                                                    shape = c(19,1),
                                                    stroke = c(0.8, 2)))) -> observed
 observed
-ggsave("/Users/timothywiemken/Library/CloudStorage/OneDrive-Pfizer/Documents/Research/github/COVID forecast/manuscript/JAMA/Figures/anomaly_flu_all_observed.pdf", width=10, height=6)
+ggsave("~/Desktop/influenza.pdf", width=14, height=6)
 
